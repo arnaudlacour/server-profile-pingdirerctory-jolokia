93f5299633adab6669aabc5d89290884b127cf6a
arno <arnaudlacour@pingidentity.com>
2021-01-08 12:24:18 -0700

    include hacks for restart

---
+++ pd-conf/hooks/181-install-extensions.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# shellcheck disable=SC1090
+. "${HOOKS_DIR}"/pingcommon.lib.sh
+. "${HOOKS_DIR}"/nice.lib.sh
+
+_manageExtension="${SERVER_ROOT_DIR}"/bin/manage-extension
+
+printf "Processing extensions.\n"
+for _extension in "${STAGING_DIR}"/extensions/*.zip
+do
+    printf "  - %s" "${_extension}"
+    printf "Installing extension [%s] ... " "${_extension}"
+    ${_manageExtension} --install "${_extension}" --no-prompt
+    _rc=${?}
+    print_status ${_rc}
+    test ${_rc} -ne 0 && exit 20
+done
+printf "All extensions processed."
+exit 0
